bc319db2e21a3c2fc272cfe5168d875a72b02d7c
Yehor Ahafonov <ratryday@gmail.com>
2023-06-28 21:05:52 +0300

    Add changes to maven.yml file

--- src/main/java/org/shop/api/controller/ItemsController.java
+++ src/main/java/org/shop/api/controller/ItemsController.java
@@ -1,21 +1,17 @@
 package org.shop.api.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
-import java.util.Objects;
-
-import static java.util.Objects.isNull;
 
 @RestController
 @RequestMapping
 public class ItemsController {
 
     @GetMapping("/items")
-    public ResponseEntity<?> getItems(@RequestParam(name = "number", value = "") Integer numb) {
+    public ResponseEntity<?> getItems(@RequestParam(name = "number") Integer numb) {
         if (numb == 0) {
             return new ResponseEntity<>("We need number",HttpStatus.BAD_REQUEST);
         }
